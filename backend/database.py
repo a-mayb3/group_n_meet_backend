78bcad75f5c7bef0ea324d3159082ee142770c1d
Borgia Leiva <edbole@campusaula.com>
2026-02-09 09:03:01 +0100

    Added checks for envvars to be defined

--- backend/database.py
+++ backend/database.py
@@ -4,10 +4,14 @@ from sqlalchemy.orm import sessionmaker
 
 from os import environ
 
+if "POSTGRES_USER" not in environ:
+    raise EnvironmentError("POSTGRES_USER environment variable not set")
+if "POSTGRES_DB" not in environ:
+    raise EnvironmentError("POSTGRES_DB environment variable not set")
 if "POSTGRES_PASSWORD" not in environ:
     raise EnvironmentError("POSTGRES_PASSWORD environment variable not set")
 
-URL_DATABASE = f"postgresql+psycopg2://group_n_meet:{environ['POSTGRES_PASSWORD']}@localhost/group_n_meet"
+URL_DATABASE = f"postgresql+psycopg2://{environ['POSTGRES_USER']}:{environ['POSTGRES_PASSWORD']}@localhost:5432/{environ['POSTGRES_DB']}"
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,34 @@
-from fastapi import FastAPI
-from routes import events
-from routes import me
+from contextlib import asynccontextmanager
+from os import environ
 
-app = FastAPI()
+from fastapi import FastAPI
+from routes import events, me
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    ## Checking if environment variables are set
+    if "POSTGRES_USER" not in environ:
+        raise EnvironmentError("POSTGRES_USER environment variable not set")
+    if "POSTGRES_DB" not in environ:
+        raise EnvironmentError("POSTGRES_DB environment variable not set")
+    if "POSTGRES_PASSWORD" not in environ:
+        raise EnvironmentError("POSTGRES_PASSWORD environment variable not set")
+
+    yield
+
+
+app = FastAPI(
+    lifespan=lifespan
+    ## TODO: Add exception handlers
+    ## TODO: Add CORS middleware
+    ## TODO: Add logging middleware
+    )
 
 app.include_router(events.router)
 app.include_router(me.router)
 
-## ...
-
+## In case this file is run directly...
 def main():
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
