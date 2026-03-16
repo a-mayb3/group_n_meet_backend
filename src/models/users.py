0382a28d731c101cb8b07272cea3dc4acfff4acb
Borgia Leiva <edbole@campusaula.com>
2026-03-16 10:53:38 +0000

    Changes str to EMailStr for email inputs in User pydantic models

--- src/models/users.py
+++ src/models/users.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import List
 
 from models.rsvps import RSVPBase
 
 class UserBase(BaseModel):
     id: str
-    email_address: str
+    email_address: EmailStr
     display_name: str
     user_type: str # REGULAR or ADMIN
 
@@ -15,5 +15,5 @@ class UserFullInfo(UserBase):
 
 class UserAuth(BaseModel):
     """Info sent by the client when logging in."""
-    email_address: str
+    email_address: EmailStr
     password: str
