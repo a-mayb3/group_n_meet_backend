ab09d1b128680813f0f7b230eaa03d344237fc3d
Edoardo Borgia Leiva <edbole@campusaula.com>
2026-03-18 12:51:58 +0100

    minor change in database.py

--- src/database.py
+++ src/database.py
@@ -1,3 +1,5 @@
+import os
+
 from typing import Annotated
 
 from fastapi import Depends
@@ -5,10 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
 
-from os import environ
-
-# TODO:  make this work with docker compose stack
-URL_DATABASE = f"postgresql+psycopg2://{environ['POSTGRES_USER']}:{environ['POSTGRES_PASSWORD']}@postgres:5432/{environ['POSTGRES_DB']}"
+URL_DATABASE = f"postgresql+psycopg2://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@postgres:5432/{os.environ['POSTGRES_DB']}"
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
